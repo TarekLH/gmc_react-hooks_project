a82a280df4b7a60e68d05d22cee7acada61ede06
TarekLH <lahouaouitarek99@gmail.com>
2022-11-13 21:42:27 +0100

    work update

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,34 +1,42 @@
-import React, {useState} from "react";
-import { MovieList } from "./MovieList";
+import React from "react";
 
 
 
-export function Filter() {
-    const [titleFilter, setFilter] = useState("")
+export default function Filter(props) {
 
     const handleTitleSearch = (event) => {
-        setFilter(event.target.value)
+        props.setFilter(event.target.value)
     }
 
-
-    let dataSearch = MovieList.filter( movie => {
-        movie.title.toLowerCase().includes(titleFilter.toLowerCase())
-        {/*Object.keys(movie).some(key => 
-            movie[key].toString().toLowerCase().includes(titleFilter.toString().toLowerCase())
-        )*/}
-    })
-
-    console.log(titleFilter)
+    console.log(props.titleFilter)
     return (
-        <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-            <input 
-            type="search" 
-            className="form-control form-control-dark text-bg-dark" 
-            placeholder="Search..." 
-            aria-label="Search"
-            value={titleFilter}
-            onChange={handleTitleSearch.bind(this)}
-        />
-        </form>
+        <>
+            <header className="p-3 text-bg-dark">
+                <div className="container">
+                    <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+
+                        <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                            <span className="fs-4">tMovies</span>
+                        </a>
+
+                        <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0"></ul>
+
+                        <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+                            <input 
+                                type="search" 
+                                className="form-control form-control-dark text-bg-dark" 
+                                placeholder="Search..." 
+                                aria-label="Search"
+                                value={props.titleFilter}
+                                onChange={handleTitleSearch.bind(this)}
+                            />
+                        </form>
+                        <div className="text-end">
+                            <button type="button" className="btn btn-warning">Add Movie</button>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
     )
 }
--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,12 +1,17 @@
-import React from "react";
-import Navbar from "./Navbar";
+import React, {useState} from "react";
+import  Filter from "./Filter";
 import MovieCard from "./MovieCard";
+import { MovieList } from "./MovieList";
 
 export default function Layout() {
+    const [titleFilter, setFilter] = useState("")
+
+    let dataSearch = MovieList.filter( movie => movie.title.toLowerCase().includes(titleFilter.toLowerCase()) )
+
     return (
         <>
-            <Navbar/>
-            <MovieCard/>
+            <Filter titleFilter={titleFilter} setFilter={setFilter}/>
+            <MovieCard dataSearch={dataSearch}/>
         </>
     )
 }
